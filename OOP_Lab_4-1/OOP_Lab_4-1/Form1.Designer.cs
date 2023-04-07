ef4484ceb6e5fda8c409e3c79e01321d3d3f767f
MerrikM <merrikm.pl03@gmail.com>
2023-04-08 01:07:54 +0500

    first commit

---
+++ OOP_Lab_4-1/OOP_Lab_4-1/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿
+namespace OOP_Lab_4_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Location = new System.Drawing.Point(13, 63);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(75, 23);
+            this.buttonDelete.TabIndex = 2;
+            this.buttonDelete.Text = "Del";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.checkBox1.AutoCheck = false;
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(13, 13);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(83, 19);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(13, 39);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(83, 19);
+            this.checkBox2.TabIndex = 4;
+            this.checkBox2.Text = "checkBox2";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1904, 1041);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.buttonDelete);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+    }
+}
+
---
+++ OOP_Lab_4-1/OOP_Lab_4-1/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//OOP_Lab_4_1
+
+
+/*
+ События KeyDown и KeyUp отправляются сфокусированному контролу. Если на форме есть другие контролы, и они сфокусированы, то они будут заглатывать эти события, и форме ничего не останется.
+
+Если вы хотите, чтобы форме всегда приходили эти события, то установите для формы KeyPreview=true.
+ 
+ */
+namespace OOP_Lab_4_1
+{
+
+    public partial class Form1 : Form
+    {
+        private int radius = 0;
+        private List<CCircle> Circles = new List<CCircle>();
+        private int ctrl = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            radius = 70;
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            foreach (CCircle Circle in Circles)
+            {
+                Circle.drawCircle(e.Graphics); 
+            }
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (ctrl == 0) 
+            {
+                foreach (CCircle Circle1 in Circles)
+                {
+                    Circle1.setColor("Black");
+                }
+                CCircle Circle = new CCircle(e.X, e.Y, radius);
+                Circles.Add(Circle);
+            }
+            if (ctrl == 1) 
+            {
+                foreach (CCircle Circle1 in Circles)
+                {
+                    if (Circle1.mouseinCircle(e) == true && checkBox2.Checked == true) 
+                    {
+                        break;
+                    }
+                }
+                Refresh();
+            }
+            Refresh();
+        }
+
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (ModifierKeys == Keys.Control) 
+            {
+                checkBox1.Checked = !checkBox1.Checked; 
+            }
+            switch (ctrl) 
+            {
+                case 0:
+                    ctrl++;
+                    foreach (CCircle Circle1 in Circles)
+                    {
+                        Circle1.setCtrl(true);
+                    }
+                    break;
+                case 1:
+                    ctrl = 0;
+                    foreach (CCircle Circle1 in Circles)
+                    {
+                        Circle1.setCtrl(false);
+                    }
+                    break;
+            }
+
+        }
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < Circles.Count(); i++)
+            {
+                if (Circles[i].getColor() == "Red")
+                {
+                    Circles.RemoveAt(i);
+                    i--;
+                }
+            }
+            Refresh();
+        }
+        public class CCircle
+        {
+            private int coordX, coordY, radius;
+            private string color = "Red";
+            private bool ctrl = false;
+            public CCircle(int coordX, int coordY, int radius)
+            {
+                this.coordX = coordX;
+                this.coordY = coordY;
+                this.radius = radius;
+
+            }
+            public void drawCircle(Graphics Canvas) 
+            {
+                if (color == "Red")
+                {
+                    Canvas.DrawEllipse(new Pen(Color.Red, radius * 2),      // Структура Pen, определяющая цвет, ширину и стиль эллипса.
+                                                       coordX - radius,     // Координата X верхнего левого угла ограничивающего прямоугольника, который определяет эллипс.
+                                                       coordY - radius,     // Координата Y верхнего левого угла ограничивающего прямоугольника, который определяет эллипс.
+                                                       radius * 2,          // Ширина ограничивающего прямоугольника, который определяет эллипс.
+                                                       radius * 2);         // Высота ограничивающего прямоугольника, который определяет эллипс.
+                }
+                else
+                {
+                    Canvas.DrawEllipse(new Pen(Color.Black, radius * 2),
+                                                       coordX - radius,
+                                                       coordY - radius,
+                                                       radius * 2,
+                                                       radius * 2);
+                }
+            }
+            public void setColor(string Color) 
+            {
+                color = Color;
+            }
+            public string getColor() 
+            {
+                return color;
+            }
+            public bool mouseinCircle(MouseEventArgs e) 
+            {
+                if (ctrl)
+                {
+                    if (Math.Pow(e.X - coordX, 2) + Math.Pow(e.Y - coordY, 2) <= Math.Pow(radius, 2) && color != "Red")
+                    {
+                        color = "Red";
+                        return true;
+                    }
+                }
+                return false;
+            }
+            public void setCtrl(bool a)
+            {
+                ctrl = a;
+            }
+
+        }
+    }
+}
